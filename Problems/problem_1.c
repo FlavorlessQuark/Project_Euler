b2f3868ada7a577b3a139bdba93ff8ef258e9dea
FlavorlessQuark <flavorlessquark@gmail.com>
2021-05-11 17:56:13 -0700

    Problem_1 Reviewed. Manual commmit

--- Problems/problem_1.c
+++ Problems/problem_1.c
@@ -17,8 +17,25 @@ int	solution_regular() // This would not work if the upper limit was higher...
 	return sum;
 }
 
+int solution_math()
+{
+	int sum3;
+	int sum5;
+	int sum15;
+
+	// sum3 = ((333 * 3) + 333 * 999) / 2;
+	// sum5 = ((199 * 5) + 199 * 995) / 2;
+	// sum15 = ((66 * 15) + 66 * 985) / 2;
+
+	sum3 = 3 * (333 * (333 + 1))/2;
+	sum5 = 5 * (199 * (199 + 1))/2;
+	sum15 = 15 * (66 * (66 + 1))/2;
+	return sum3 + sum5 - sum15;
+}
+
 int main(void)
 {
 	printf("Solution : %d\n", solution_regular());
+	printf("Solution : %d\n", solution_math());
 	return 0;
 }
